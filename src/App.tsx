fb1b1e885cc89d91096eead4ba019f42350b1d4b
IgnacioMoyano559 <ignaciomoyano559@gmail.com>
2025-09-29 17:36:09 -0300

    UpdateData

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import './App.css'; 
 import Login from './pages/Login';
 import RecoverPassword from './pages/RecoverPassword';
+import UpdateData from './pages/UpdateData';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -10,9 +12,10 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<UpdateData />} />
           <Route path="/iniciarSesion" element={<Login />} />
           <Route path="/recuperar" element={<RecoverPassword />} />
+          <Route path="/actualizarInformacion" element={<UpdateData />} />
         </Routes>
       </div>
     </Router>
