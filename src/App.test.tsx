4f0372ee6477d51f08b093a7c6e320e7c9db9a8c
IgnacioMoyano559 <ignaciomoyano559@gmail.com>
2025-09-30 19:33:27 -0300

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,3 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+test('renders App component', () => {
+  render(<App />);
+  // Replace this with a relevant assertion for your App
+  expect(screen.getByText(/Foraria/i)).toBeInTheDocument();
+});
