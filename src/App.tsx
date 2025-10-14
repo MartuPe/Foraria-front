74c97e8f32fe215527094efacd34643d869fc678
IgnacioMoyano559 <ignaciomoyano559@gmail.com>
2025-10-14 15:38:53 -0300

    botonesIniciarSesionAdmin y login centrado

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ import AdminLayout from "./components/layout/AdminLayout";
 import AdminReclaims from "./pages/admin/AdminReclaims";
 import AdminForums from "./pages/admin/AdminForums";
 import AdminAudit from "./pages/admin/AdminAudit";
-import UserManagment from "./pages/admin/UserManagement";
+import AdminUserManagment from "./pages/admin/AdminUserManagement";
 import AdminVotes from "./pages/admin/AdminVotes";
 function App() {
   return (
@@ -92,7 +92,7 @@ function App() {
             <Route path="reclaims" element={<AdminReclaims />} />
             <Route path="forums" element={<AdminForums />} />
             <Route path="audit" element={<AdminAudit />} />
-            <Route path="gestionUsuario" element={<UserManagment />} />
+            <Route path="gestionUsuario" element={<AdminUserManagment />} />
             <Route path="votaciones" element={<AdminVotes/>} />
             {/* AGREGAR rutas de proveedores para admin */}
             <Route path="suppliers" element={<Suppliers />} />
